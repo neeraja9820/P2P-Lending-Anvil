a0b4aeea63e7519058ee464e4db8e7ef45d273c6
neeraja.bhupathi1696@gmail.com <neeraja.bhupathi1696@gmail.com>
2024-05-28 10:57:31 +0000

    Edited lendor.lendor_registration_forms.lender_registration_form_2.lender_registration_Institutional_form_2

--- client_code/lendor/lendor_registration_forms/lender_registration_form_2/lender_registration_Institutional_form_2/__init__.py
+++ client_code/lendor/lendor_registration_forms/lender_registration_form_2/lender_registration_Institutional_form_2/__init__.py
@@ -78,7 +78,11 @@ class lender_registration_Institutional_form_2(lender_registration_Institutional
     """This method is called when a new file is loaded into this FileLoader"""
     if file:
       self.image_1.source = self.file_loader_1.file
-
-  def date_picker_1_change(self, **event_args):
-    """This method is called when the selected date changes"""
-    self.date_picker_1.date = datetime.now().date()
+      
+  def check_due_date_alert(self, due_date):
+        try:
+            current_date = datetime.now().date()
+            if due_date < current_date:
+                Notification("The due date has passed!").show()
+        except Exception as e:
+            Notification("An error occurred while checking due date: {}".format(str(e))).show()  
