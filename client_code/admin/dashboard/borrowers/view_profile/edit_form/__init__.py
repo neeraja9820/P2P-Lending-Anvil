8d6a30dabc2e188f2101ae7778064cd2b59d9bee
neeraja.bhupathi1696@gmail.com <neeraja.bhupathi1696@gmail.com>
2024-06-06 14:24:41 +0000

    Edited admin.dashboard.borrowers.view_profile.edit_form

--- client_code/admin/dashboard/borrowers/view_profile/edit_form/__init__.py
+++ client_code/admin/dashboard/borrowers/view_profile/edit_form/__init__.py
@@ -400,20 +400,7 @@ class edit_form(edit_formTemplate):
         # data.update()
         print(f"Updated user profile, borrower, foreclosure, extends loan, loand etails and wallet table for customer_id: {self.get}")
         open_form('admin.dashboard.borrowers.view_profile', self.get)
-  def calculate_dob_from_age(self, age):
-      """Calculate date of birth from age"""
-      today = date.today()
-      year_of_birth = today.year - age
-      dob = date(year_of_birth, today.month, today.day)
-      # Adjust if the birthdate is not valid (e.g., leap year)
-      while True:
-        try:
-          dob = dob.replace(year=year_of_birth)
-          break
-        except ValueError:
-          year_of_birth -= 1
-      return dob    
-
+ 
   # def button_2_click(self, **event_args):
   #   """This method is called when the button is clicked"""
   #   data = tables.app_tables.fin_user_profile.search()
