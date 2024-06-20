70a3c465a139ae33cecd9daeb51ba56ded6bebdc
neeraja.bhupathi1696@gmail.com <neeraja.bhupathi1696@gmail.com>
2024-06-20 12:40:54 +0000

    Edited admin.dashboard.accounting.performance_tracker.open_loans

--- client_code/admin/dashboard/accounting/performance_tracker/open_loans/__init__.py
+++ client_code/admin/dashboard/accounting/performance_tracker/open_loans/__init__.py
@@ -16,7 +16,7 @@ class open_loans(open_loansTemplate):
 
     # Any code you write here will run before the form opens.
     # Fetch approved loans data from fin_loan_details table
-    self.data = app_tables.fin_loan_details.search(loan_updated_status=q.like('disbursed%'))
+    self.data = app_tables.fin_loan_details.search(loan_updated_status=q.any_of("disbursed%" , "extension"))
     # self.data = app_tables.fin_loan_details.search((q.like('extention%')) & (q.like('disbursed%')))
     # self.data = app_tables.fin_loan_details.search(
     #         (q.like('loan_updated_status', 'extention%')) & 
