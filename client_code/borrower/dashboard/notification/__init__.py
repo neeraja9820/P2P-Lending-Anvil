7434c85af999e9d9a4d033e128be607d5241b207
neeraja.bhupathi1696@gmail.com <neeraja.bhupathi1696@gmail.com>
2024-06-25 11:05:32 +0000

    Edited borrower.dashboard.application_tracker, borrower.dashboard.borrower_portfolio, borrower.dashboard.borrower_portfolio_first_page, borrower.dashboard.borrower_profile and 15 more

--- client_code/borrower/dashboard/notification/__init__.py
+++ client_code/borrower/dashboard/notification/__init__.py
@@ -35,6 +35,4 @@ class notification(notificationTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('borrower.dashboard')
+
